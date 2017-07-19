a49a416ba9d9ebfb5bb8014610b0a9fe8ff2f8b6
henser <741515435@qq.com>
2017-07-19 11:28:34 +0800

    this ia a test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learn git
+
+#this a modify
---
+++ test.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: test.c
+    > Author: xxx
+    > Mail: XXX.com
+    > Created Time: 2017年07月19日 星期三 11时09分45秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello !\n");
+    return 0;
+}
